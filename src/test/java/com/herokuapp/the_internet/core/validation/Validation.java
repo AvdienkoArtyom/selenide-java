ac4387f9afba284c74636bcceaca546ed66d1b91
artem <a.avdienko@pflb.ru>
2022-01-26 15:56:57 +0300

    Version 6

--- src/test/java/com/herokuapp/the_internet/core/validation/Validation.java
+++ src/test/java/com/herokuapp/the_internet/core/validation/Validation.java
@@ -119,6 +119,9 @@ public class Validation {
         }
     }
 
+
+
+
     @Step("Сравнение скачаного файла")
     public static void checkTwoFile(Path expected, Path actual) {
         ComparingTwoFiles.comparing(expected, actual);
